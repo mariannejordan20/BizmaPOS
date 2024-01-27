555a50daa0f092a821470e73b2713edefc53993e
mariannejordan20 <157771863+mariannejordan20@users.noreply.github.com>
2024-01-27 10:01:00 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -26,62 +26,49 @@
     <div class="sidebar-heading">
         Main
     </div>
+   
     <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-        aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-soccer-ball-o"></i>
-        <span>Specific Sport</span>
-    </a>
-    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-        data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">List of Sports:</h6>
-            <a class="collapse-item" href="sports.php?sport=arnis">Arnis</a>
-            <a class="collapse-item" href="sports.php?sport=athletics">Athletics</a>
-            <a class="collapse-item" href="sports.php?sport=badminton">Badminton</a>
-            <a class="collapse-item" href="sports.php?sport=baseball">Baseball</a>
-            <a class="collapse-item" href="sports.php?sport=basketball">Basketball</a>
-            <a class="collapse-item" href="sports.php?sport=chess">Chess</a>
-            <a class="collapse-item" href="sports.php?sport=dancesports">Dancesports</a>
-            <a class="collapse-item" href="sports.php?sport=futsal">Futsal</a>
-            <a class="collapse-item" href="sports.php?sport=football">Football</a>
-            <a class="collapse-item" href="sports.php?sport=softball">Softball</a>
-            <a class="collapse-item" href="sports.php?sport=swimming">Swimming</a>
-            <a class="collapse-item" href="sports.php?sport=sepak-takraw">Sepak Takraw</a>
-            <a class="collapse-item" href="sports.php?sport=table-tennis">Table Tennis</a>
-            <a class="collapse-item" href="sports.php?sport=taekwando-koryugi">Taekwando Koryugi</a>
-            <a class="collapse-item" href="sports.php?sport=taekwando-poomsae">Taekwando Poomsae</a>
-            <a class="collapse-item" href="sports.php?sport=tennis">Tennis</a>
-            <a class="collapse-item" href="sports.php?sport=volleyball">Volleyball</a>
-        </div>
-    </div>
-</li>  
-<li class="nav-item">
-        
+    
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="products.php">
+            <i class="fas fa-shopping-cart"></i>
+            <span>Sales</span>
+        </a>
+
         <li class="nav-item">
         <a class="nav-link collapsed" href="products.php">
             <i class="fas fa-fw fa-users"></i>
-            <span>All products</span>
+            <span>Products</span>
         </a>
-   
-   
-    
- 
-   
 
-    
-   
-   
-       
+         <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseStock"
+                aria-expanded="true" aria-controls="collapseUtilities">
+                <i class="fas fa-fw fa-soccer-ball-o"></i>
+                <span>Stock</span>
+            </a>
+            <div id="collapseStock" class="collapse" aria-labelledby="headingUtilities"
+                data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <!-- <h6 class="collapse-header">List of Sports:</h6> -->
+                    <a class="collapse-item" href="sports.php?sport=arnis">Item Categories</a>
+                    <a class="collapse-item" href="sports.php?sport=athletics">Item Sub-categories</a>
+                    <a class="collapse-item" href="sports.php?sport=badminton">Item Units</a>
+                </div>
+            </div>
+        </li>  
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="products.php">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Customers</span>
+        </a>
 
-    
-        
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="products.php">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Expenses</span>
+        </a>
 
-    <!-- Divider -->
-   
-
-    <!-- Heading -->
-    
     
 
     <!-- Divider -->
