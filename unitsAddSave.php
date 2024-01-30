f3d72458e3073c1d6508e1196fdae38ece21c22a
narcisoangelo62 <131828059+narcisoangelo62@users.noreply.github.com>
2024-01-30 11:15:50 +0800

    Working addunits

---
+++ unitsAddSave.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+	include('connection.php');
+
+   
+    $unitname = $_POST['unit_name'];
+    
+        
+
+$query = mysqli_query($conn,"SELECT * FROM units WHERE unit_name = '$unitname'");
+
+$msg = "";
+ 
+
+
+if (mysqli_num_rows($query)>0)
+
+{
+
+	$_SESSION['status'] = "Unit already exists!";
+    $_SESSION['status_code'] = "error";
+	header("location: ".$_SERVER['HTTP_REFERER']);
+}
+
+else{
+    
+
+    
+
+$add = "insert into units set unit_name = '".$unitname."'";
+
+$res = $conn->query($add);
+
+
+
+	if ($res){
+
+		$_SESSION['status'] = "Unit Information Saved";
+        $_SESSION['status_code'] = "success";
+        header("location:index.php");
+
+		
+	
+}
+}
+
+
+
+
+?>
+
+
+
+
+
+
+
+
