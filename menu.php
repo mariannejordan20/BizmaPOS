894ece3040445b68c3c50ef228b7e96f4557dd92
mariannejordan20 <157771863+mariannejordan20@users.noreply.github.com>
2024-01-30 10:21:32 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,7 +5,7 @@ include('connection.php');
 ?>
 
 <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color: #313a46;">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
