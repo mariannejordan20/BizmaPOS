404e9ad4a80db7d68828d441876aa6bfe1418121
CristobalParaon25 <paraoncristobal25@gmail.com>
2024-02-01 10:19:34 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand navbar-light  topbar mb-4 static-top shadow" style="background-color: #2f3742">
+<nav class="navbar navbar-expand navbar-light  topbar mb-4 static-top shadow" style="background-color: white">
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
     </button>
