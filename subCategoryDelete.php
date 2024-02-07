c02391322b74bd6510a92da15fb12477ca167357
CristobalParaon25 <paraoncristobal25@gmail.com>
2024-02-07 15:00:16 +0800

    updated sales.php

--- subCategoryDelete.php
+++ subCategoryDelete.php
@@ -3,7 +3,7 @@ session_start();
 include('connection.php');
 
 // Use $_GET to retrieve the sub_category parameter
-$subcat = $_GET['sub_category'];
+$subcat = $_GET['subCategoriesTable'];
 
 // Use prepared statement to avoid SQL injection
 $delete = $conn->prepare("DELETE FROM subcategories WHERE sub_category = ?");
